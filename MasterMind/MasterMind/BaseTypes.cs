9441d75a70e45996c439804016c0ed1a7def4750
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-10-18 00:17:33 +0200

    problem setup

---
+++ MasterMind/MasterMind/BaseTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace MasterMind
+{
+    public enum CodePeg
+    {
+        White,
+        Purple,
+        Yellow,
+        Green,
+        Red,
+        Blue
+    }
+    public enum KeyPeg
+    {
+        Black,
+        White
+    }
+}
---
+++ MasterMind/MasterMind/ICodeAnalyzer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MasterMind
+{
+    public interface ICodeAnalyzer
+    {
+        ICollection<KeyPeg> EvaluateGuess(IList<CodePeg> guess, IList<CodePeg> code);
+    }
+}
