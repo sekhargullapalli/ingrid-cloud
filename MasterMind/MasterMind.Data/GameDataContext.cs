4e80571313406788d7400c3aeb0a1d6ed4bdae63
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-10-18 11:46:04 +0200

    data context added

--- MasterMind/MasterMind.Data/GameDataContext.cs
+++ MasterMind/MasterMind.Data/GameDataContext.cs
@@ -15,8 +15,41 @@ namespace MasterMind.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);           
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.CodePeg1)
+               .HasConversion(GamePattern.GetCodePegConverter());
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.CodePeg2)
+               .HasConversion(GamePattern.GetCodePegConverter());
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.CodePeg3)
+               .HasConversion(GamePattern.GetCodePegConverter());
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.CodePeg4)
+               .HasConversion(GamePattern.GetCodePegConverter());
+
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.KeyPeg1)
+               .HasConversion(GamePattern.GetKeyPegConverter());
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.KeyPeg2)
+               .HasConversion(GamePattern.GetKeyPegConverter());
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.KeyPeg3)
+               .HasConversion(GamePattern.GetKeyPegConverter());
+            modelBuilder.Entity<GamePattern>()
+               .Property(gp => gp.KeyPeg4)
+               .HasConversion(GamePattern.GetKeyPegConverter());
+
+            modelBuilder.Entity<GamePattern>(entity =>
+            {
+                entity.HasKey(e => e.Game);
+                entity.HasOne(g => g.Game)
+                .WithMany(p => p.Patterns)
+                .OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 }
