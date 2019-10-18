efb79b09140d79eb3e9cd72592debf3b644508df
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-10-18 04:21:07 +0200

    data context added

---
+++ MasterMind/MasterMind.Data/GameDataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasterMind.Data
+{
+    class GameDataContext: DbContext
+    {
+        public GameDataContext(DbContextOptions<GameDataContext> options)
+        : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);           
+
+        }
+    }
+}
