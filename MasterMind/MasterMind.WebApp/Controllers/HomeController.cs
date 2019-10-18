6ca1b082d944fc78afd9127c3f0d77bc953512f2
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-10-18 02:54:12 +0200

    basic web app layout added

--- MasterMind/MasterMind.WebApp/Controllers/HomeController.cs
+++ MasterMind/MasterMind.WebApp/Controllers/HomeController.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MasterMind.WebApp.Models;
+using MasterMind;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MasterMind.WebApp.Controllers
-{
+{    
     public class HomeController : Controller
     {
+        private ICodeAnalyzer _codeAnalyzer;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICodeAnalyzer codeAnalyzer)
         {
             _logger = logger;
+            _codeAnalyzer = codeAnalyzer;
         }
 
         public IActionResult Index()
@@ -23,15 +27,20 @@ namespace MasterMind.WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+     
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [AllowAnonymous]
+        public IActionResult NotFoundRoute(string id)
+        {
+            Response.StatusCode = 404;
+            id = id.Replace("*^S^*", "/");
+            ViewData["OriginalRoute"] = id;
+            return View();
         }
     }
 }
--- MasterMind/MasterMind.WebApp/Startup.cs
+++ MasterMind/MasterMind.WebApp/Startup.cs
@@ -20,15 +20,25 @@ namespace MasterMind.WebApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<ICodeAnalyzer>(new CodeAnalyzer());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    string opath = context.Request.Path;
+                    opath = opath.Replace("/", "*^S^*");
+                    context.Request.Path = "/Home/NotFoundRoute/" + opath;
+                    await next();
+                }
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,16 +46,12 @@ namespace MasterMind.WebApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
