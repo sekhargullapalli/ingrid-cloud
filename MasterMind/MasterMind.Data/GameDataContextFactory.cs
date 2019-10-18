7bf0bd1ca0aa5805af53652507ac831c42c919d9
Vijaya Sekhar Gullapalli <vijaya.sekhargullapalli@swep.net>
2019-10-18 11:55:14 +0200

    data context factory added

---
+++ MasterMind/MasterMind.Data/GameDataContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MasterMind.Data
+{   
+    public class GameDataContextFactory : IDesignTimeDbContextFactory<GameDataContext>
+    {
+        public GameDataContextFactory() { }
+
+        //For testing, using sqlite
+        public GameDataContext CreateDbContext(string[] args)
+        {
+            string connectionString = @"Data Source=gamedb.db";
+            var optionsBuilder = new DbContextOptionsBuilder<GameDataContext>();
+            optionsBuilder.UseSqlite(connectionString);
+            return new GameDataContext(optionsBuilder.Options);
+        }
+    }
+}
