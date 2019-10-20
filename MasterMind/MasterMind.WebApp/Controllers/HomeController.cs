48d10d28e76168b2dc972e8f4791f7bef6e94416
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-10-20 15:24:02 +0200

    browser back event handled

--- MasterMind/MasterMind.WebApp/Controllers/HomeController.cs
+++ MasterMind/MasterMind.WebApp/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ using MasterMind.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace MasterMind.WebApp.Controllers
-{    
+{
     public class HomeController : Controller
     {
         private ICodeAnalyzer _codeAnalyzer;
@@ -41,7 +41,7 @@ namespace MasterMind.WebApp.Controllers
                 CodePeg2 = SiteExtensions.RandomEnum<CodePeg>(),
                 CodePeg3 = SiteExtensions.RandomEnum<CodePeg>(),
                 CodePeg4 = SiteExtensions.RandomEnum<CodePeg>(),
-                KeyPeg1= KeyPeg.None,
+                KeyPeg1 = KeyPeg.None,
                 KeyPeg2 = KeyPeg.None,
                 KeyPeg3 = KeyPeg.None,
                 KeyPeg4 = KeyPeg.None,
@@ -58,9 +58,9 @@ namespace MasterMind.WebApp.Controllers
             Game game = null;
             try
             {
-                game = _context.Games.AsNoTracking().Include(p=>p.Patterns).Where(g => g.Guid == model.guid).FirstOrDefault();                
+                game = _context.Games.AsNoTracking().Include(p => p.Patterns).Where(g => g.Guid == model.guid).FirstOrDefault();
                 int currlevel = game.Patterns.Count();
-                if (game.IsCompleted || model.level<currlevel)
+                if (game.IsCompleted || model.level < currlevel)
                     return View(game);
                 var basePatern = game.Patterns.Where(p => p.Level == 0).FirstOrDefault();
                 List<CodePeg> code = new List<CodePeg>
@@ -79,14 +79,14 @@ namespace MasterMind.WebApp.Controllers
                 };
                 List<KeyPeg> keys = _codeAnalyzer.EvaluateGuess(guess, code).ToList();
                 List<KeyPeg> _keys = new List<KeyPeg>();
-                foreach(var key in keys)
+                foreach (var key in keys)
                     _keys.Add(key);
                 while (_keys.Count < 4)
                     _keys.Add(KeyPeg.None);
 
                 var pattern = new GamePattern
                 {
-                    Level=currlevel,
+                    Level = currlevel,
                     CodePeg1 = guess[0],
                     CodePeg2 = guess[1],
                     CodePeg3 = guess[2],
@@ -95,23 +95,34 @@ namespace MasterMind.WebApp.Controllers
                     KeyPeg2 = _keys[1],
                     KeyPeg3 = _keys[2],
                     KeyPeg4 = _keys[3],
-                    Game =game
+                    Game = game
                 };
                 game.Patterns.Add(pattern);
-                game.IsCompleted = pattern.IsCompleted() ||model.level==8;
+                game.IsCompleted = pattern.IsCompleted() || model.level == 8;
                 _context.Games.Update(game);
-                _context.GamePatterns.Add(pattern);                           
+                _context.GamePatterns.Add(pattern);
                 _context.SaveChanges();
             }
             catch (Exception)
             {
-                if(game==null)
+                if (game == null)
                     RedirectToAction(nameof(HomeController.Index), "Home");
                 return
                     View(game);
-            }            
+            }
             return View(game);
-        }     
+        }
+        [HttpGet("/GetCurrLevel/{gameid}")]
+        public JsonResult GetCurrLevel(string gameid)
+        {
+            Game game = null;
+            try
+            {
+                game = _context.Games.AsNoTracking().Include(p => p.Patterns).Where(g => g.Guid == gameid).FirstOrDefault();
+                return Json(game.Patterns.Count());
+            }
+            catch (Exception) { return Json(0); }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
